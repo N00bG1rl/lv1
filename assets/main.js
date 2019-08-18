e95785f3020e06d47db30794e57b44f4b6a4d4c1
Helen <helen7089@gmail.com>
2019-08-18 21:43:37 +0300

    Updates

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,20 @@ function closeNav() {
 // CONTACT FORM
 // Auto expand #contact form textarea
 function adjust_textarea(h) {
-  h.style.height = "45px";
+  h.style.height = "80px";
   h.style.height = (h.scrollHeight)+"px";
 }
+
+function toggleContent() {
+  var main = document.getElementById('main');
+  main.classList.toggle('open');
+}
+
+/* function openContent() {
+  document.getElementById('main').style.height = '250px';
+  document.getElementById('handle-wrapper').style.display = 'none';
+}
+function closeContent() {
+  document.getElementById('main').style.display = 'none';
+  document.getElementById('handle-wrapper').style.display = 'block';
+} */
