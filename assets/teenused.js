cb0d430081df46fe07c71eadf36dac58be42b544
Helen <helen7089@gmail.com>
2019-09-12 05:37:32 +0300

    Services popup content

---
+++ assets/teenused.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  /* responsiveSlides plugin */
+  $(function() {
+    $(".rslides").responsiveSlides({
+      nav: true,
+      prevText: '<i class="pe-7s-angle-left"></i>',
+      nextText: '<i class="pe-7s-angle-right"></i>',
+      pause: true,
+    });
+  });
+
+  /* MagnifPopup Plugin */
+  $('.popup-with-zoom-anim').magnificPopup({
+      type: 'inline',
+      fixedContentPos: true,
+      fixedBgPos: true,
+      overflowY: 'auto',
+      closeBtnInside: true,
+      preloader: false,
+      midClick: true,
+      removalDelay: 300,
+      mainClass: 'my-mfp-zoom-in' // close btn
+  });
+});
