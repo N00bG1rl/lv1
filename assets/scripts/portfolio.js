bd2bb0fc972d4503c2eab20ddbfe78c47c020c98
Helen <helen7089@gmail.com>
2019-09-17 07:01:30 +0300

    Portfolio grid

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -1,47 +1,60 @@
-// TEHTUD GRID FILTER
 
-// Filter #tehtud grid elements
-filterSelection('all')
-function filterSelection(element) {
-  var x, i;
-  x = document.getElementsByClassName("grid-item");
-  if (element == "all")
-    element = "";
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(element) > -1) addClass(x[i], "show");
-  }
-}
-// Show elements that is selected
-function addClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-// Hide elements that is not selected
-function removeClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-// Add active class to the current select
-var listContainer = document.getElementById("list-container");
-var list = listContainer.getElementsByClassName("list");
-for (var i = 0; i < list.length; i++) {
-  list[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+// magnigic-popup Plugin
+$(document).ready(function() {
+  $('.gallery').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled:true,
+      navigateByImgClick: true,
+      tPrev: 'Previous (Left arrow key)',
+      tNext: 'Next (Right arrow key)',
+      tCounter: '<span class="mfp-counter">%curr% of %total%</span>'
+    },
   });
-}
+});
+
+/* jQuery('#grid-container').cubeportfolio({
+  filters: '#filters-container', // you can even add multiple filters here (e.g. `filters: '#filters-container1, #filters-container2, #filters-container3'`)
+}); */
+
+(function($, window, document, undefined) {
+  'use strict';
+
+  // init cubeportfolio
+  $('#js-grid-masonry').cubeportfolio({
+      filters: '#js-filters-masonry',
+      layoutMode: 'grid',
+      defaultFilter: '*',
+      animationType: 'quicksand',
+      gapHorizontal: 20,
+      gapVertical: 20,
+      gridAdjustment: 'responsive',
+      mediaQueries: [{
+          width: 1500,
+          cols: 5,
+      }, {
+          width: 1100,
+          cols: 4,
+      }, {
+          width: 800,
+          cols: 3,
+      }, {
+          width: 480,
+          cols: 2,
+          options: {
+              caption: '',
+              gapHorizontal: 10,
+              gapVertical: 10,
+          }
+      }],
+      caption: 'overlayBottomAlong',
+      displayType: 'bottomToTop',
+      displayTypeSpeed: 100,
+
+      // lightbox
+      lightboxDelegate: '.cbp-lightbox',
+      lightboxGallery: true,
+      lightboxTitleSrc: 'data-title',
+      lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} / {{total}}</div>',
+  });
+})(jQuery, window, document);
