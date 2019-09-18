f4be47c3abf59b79e23794e8b116b13ad0aaf778
Helen <helen7089@gmail.com>
2019-09-18 09:29:22 +0300

    Added scripts

---
+++ assets/scripts/contact.js
@@ -0,0 +1,71 @@
+// Auto expand #contact form textarea
+function adjust_textarea(h) {
+  h.style.height = "80px";
+  h.style.height = (h.scrollHeight)+"px";
+}
+
+function toggleContent() {
+  var mainContent = document.getElementById('main-content');
+  var rotate = document.getElementById('rightArrow');
+  let txtRotate = document.getElementById('txt-rotate');
+
+  mainContent.classList.toggle('open');
+  rotate.classList.toggle('imgRotate');
+  txtRotate.classList.toggle('txtNone');
+}
+
+// Contact form validation
+$(function () {
+  $('.contact-form form .submit').on('click', function() {
+    
+    $('.contact-form form .form-control').removeClass("errorForm");
+    $('.msg_sucess,.msg_error').css("display","");
+
+    var error = false,
+        name = $('.contact-form form input[type="text"]');
+
+    if (name.val() === "" || name.val() === " ") {
+      error = true;
+      $(name).addClass("errorForm");
+    }
+
+    var email_compare = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+    email = $('.contact form input[type="email"]');
+
+    if (email.val() === "" || email.val() === " ") {
+      $(email).addClass("errorForm");
+      error = true;
+    } else if (!email_compare.test(email.val())) {
+      $(email).addClass("errorForm");
+      error = true;
+    }
+
+    var msg = $('.contact-form form textarea');
+
+    if (msg.val() === "" || msg.val() === " ") {
+      error = true;
+      $(msg).addClass("errorForm");
+    }
+
+    if (error === true) {
+      return false;
+    }
+
+    var data_string = $('.contact-form form').serialize();
+
+    $.ajax({
+      type: "POST",
+      url: $('.contact-form form').attr('action'),
+      data: data_string,
+
+      sucess: function (message) {
+        if (message === 'SENDING') {
+          $('.msg_sucess').fadeIn('slow');
+        } else {
+          $('.msg_error').fadeIn('slow');
+        }
+      }
+    });
+    return false;
+  });
+});
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,75 +7,78 @@ function closeNav() {
   document.getElementById('mobile-nav').style.width = '0';
 }
 
-// CONTACT FORM
-// Auto expand #contact form textarea
-function adjust_textarea(h) {
-  h.style.height = "80px";
-  h.style.height = (h.scrollHeight)+"px";
+/* function toggleMobilenav() {
+  var mobileNav = document.getElementById('open');
+  mobileNav.addClass('over');
+} */
+
+/* var body = document.body,
+    overlay = document.querySelector('.overlay'),
+    overlayBtts = document.querySelectorAll('div[class$="overlay"]');
+
+[].forEach.call(overlayBtts, function(btt) {
+
+  btt.addEventListener('click', function() { 
+
+     
+     var overlayOpen = this.className === 'open-overlay';
+
+     
+     overlay.setAttribute('aria-hidden', !overlayOpen);
+     body.classList.toggle('noscroll', overlayOpen);
+
+     
+     overlay.scrollTop = 0;
+
+  }, false);
+
+}); */
+
+/* let overlayShown = false;
+let scrollPosition = 0;
+
+document.querySelector('.open-overlay').addEventListener('click', function() {
+  if (overlayShown) {
+		showOverlay();
+  } else {
+    removeOverlay();
+  }
+  overlayShown = !overlayShown;
+});
+
+function showOverlay() {
+    scrollPosition = window.pageYOffset;
+  	const mainEl = document.querySelector('.main-content');
+    mainEl.style.top = -scrollPosition + 'px';
+  	document.body.classList.add('show-overlay');
 }
 
-function toggleContent() {
-  var mainContent = document.getElementById('main-content');
-  var rotate = document.getElementById('rightArrow');
-  let txtRotate = document.getElementById('txt-rotate');
+function removeOverlay() {
+		document.body.classList.remove('show-overlay');
+  	window.scrollTo(0, scrollPosition);
+    const mainEl = document.querySelector('.main-content');
+    mainEl.style.top = 0;
+} */
 
-  mainContent.classList.toggle('open');
-  rotate.classList.toggle('imgRotate');
-  txtRotate.classList.toggle('txtNone');
-}
-
-// Contact form validation
-$(function () {
-  $('.contact-form form .submit').on('click', function() {
-    
-    $('.contact-form form .form-control').removeClass("errorForm");
-    $('.msg_sucess,.msg_error').css("display","");
-
-    var error = false,
-        name = $('.contact-form form input[type="text"]');
-
-    if (name.val() === "" || name.val() === " ") {
-      error = true;
-      $(name).addClass("errorForm");
-    }
-
-    var email_compare = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
-    email = $('.contact form input[type="email"]');
-
-    if (email.val() === "" || email.val() === " ") {
-      $(email).addClass("errorForm");
-      error = true;
-    } else if (!email_compare.test(email.val())) {
-      $(email).addClass("errorForm");
-      error = true;
-    }
-
-    var msg = $('.contact-form form textarea');
-
-    if (msg.val() === "" || msg.val() === " ") {
-      error = true;
-      $(msg).addClass("errorForm");
-    }
-
-    if (error === true) {
-      return false;
-    }
-
-    var data_string = $('.contact-form form').serialize();
-
-    $.ajax({
-      type: "POST",
-      url: $('.contact-form form').attr('action'),
-      data: data_string,
-
-      sucess: function (message) {
-        if (message === 'SENDING') {
-          $('.msg_sucess').fadeIn('slow');
-        } else {
-          $('.msg_error').fadeIn('slow');
-        }
-      }
-    });
-    return false;
+$(function() {
+  /*This is where we specify what button is being clicked to open the 
+  overlay, change at will.*/
+  $('#open-icon').click(function() {    
+  /*This is where we specify what part of the page is gonna disable the 
+  scroll, in this case the body.*/
+      $('html')
+        .css('overflow', 'hidden');
   });
+});
+  
+  /*Now we re-enable the scroll*/
+$(function() {
+  /*This is where we specify the the part of the overlay that is being 
+  clicked to close it, in this case, the body and the .close, change at will.*/
+  $('.close-icon').click(function() {
+  /* This is where we specify the part of the page we are re-enabling 
+  the scroll, in this case the body. */
+      $('html')
+          .css('overflow-y', 'visible');
+     });
 });
